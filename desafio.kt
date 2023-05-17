6a81213c56d9a2f27f37eb27eaa451768ea00358
magnumcs <magnumcs@outlook.com>
2023-05-17 14:45:56 -0300

    Commint primeiro desafio DIO Kotlin

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,44 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, val cpf: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val nivel: Nivel, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val usuario1 = Usuario("Harry Potter", "09655788888")
+
+    val cont1 = ConteudoEducacional("Logica", Nivel.BASICO)
+    val cont2 = ConteudoEducacional("Programacao", Nivel.INTERMEDIARIO)
+    val cont3 = ConteudoEducacional("Estrutura de Dados", Nivel.DIFICIL)
+
+    val listCont: List<ConteudoEducacional> = mutableListOf(cont1, cont2, cont3)
+
+    val form1 = Formacao("Dev", listCont)
+
+    form1.matricular(usuario1)
+
+    println("nome: ${form1.nome}")
+    print("conteúdos: ")
+    form1.conteudos.forEach {
+        println(it.nome)
+        println(it.nivel)
+        println(it.duracao)
+    }
+    print("inscritos: ")
+    form1.inscritos.forEach {
+        println(it.nome)
+        println(it.cpf)
+    }
+
 }
